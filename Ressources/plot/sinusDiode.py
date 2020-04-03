80211f009f9e77b6291f8aa5aa72f704c7e9d17a
guvanderrest <guillaume.vanderrest@student.uclouvain.be>
2020-04-03 13:41:30 +0200

    omega changed to frequency

--- Ressources/plot/sinusDiode.py
+++ Ressources/plot/sinusDiode.py
@@ -12,9 +12,10 @@ import numpy as np
 # plt.plot(vt,arcsin,'r')
 # plt.plot(vt,gain*vt)
 # plt.show()
-omegaj = np.logspace(-1,5,250)
+fj = np.logspace(-1,5,250)
+omegaj = 2*np.pi*fj
 bode = (1j*omegaj)*1.6/((1+ (1j*omegaj)*0.016)*(1+(1j*omegaj)*5.3*10**-5))
 bodeAmp = 20*np.log10(np.abs(bode))
 bodePhase = np.angle(bode)
-np.savetxt('out.txt',np.transpose(np.array([omegaj,bodeAmp,bodePhase])),delimiter='\t')
+np.savetxt('out.txt',np.transpose(np.array([fj,bodeAmp,bodePhase])),delimiter='\t')
 
