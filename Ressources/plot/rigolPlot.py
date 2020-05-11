1097a0f01966073d65998c393735233e64ce7911
funnybr0ther <guillaumevdrest@gmail.com>
2020-05-11 17:20:42 +0200

    updated spice simulation

--- Ressources/plot/rigolPlot.py
+++ Ressources/plot/rigolPlot.py
@@ -1,11 +1,19 @@
-from numpy import *
+import numpy as np
 
-file = open("coolsinus.csv","r")
-outfile = open("coolsinuscool.csv",w)
-lines  = file.readlines
-secondLine = lines[1].split(",")
-start = secondLine[3]
-inc = secondLine[4]
+file = open("C:/Users/guill/Programmes/LELEC1101-31/Ressources/plot/coolsinus.csv","r")
+outfile = open("C:/Users/guill/Programmes/LELEC1101-31/Ressources/plot/coolsinuscool.csv","w")
+lines  = file.readlines()
+lineout = "In\tv1\tv2\n"
+outfile.write(lineout)
+secondLine = (lines[1]).split(",")
+start = float(secondLine[3])
+inc = float(secondLine[4])
 length = len(lines)-2
 arr = np.linspace(start,start + length*inc,length)
-for line in file.readlines[2:]:
+arrcount = 0;
+for line in lines[2:]:
+    ch1 = line.split(",")[1]
+    ch2 = line.split(",")[2]
+    newLine = str(arr[arrcount]) + "\t" + ch1 + "\t" + ch2 
+    arrcount+=1
+    outfile.write(newLine + "\n")
